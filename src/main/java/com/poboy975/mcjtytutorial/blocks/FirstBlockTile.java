3a83ddf2a5807ad0fef8f6608329313681ff4722
Durlon Bryant <durlon@hotmail.com>
2019-07-17 19:13:06 -0500

    Corrected lazyoption, so it's properly cached now

--- src/main/java/com/poboy975/mcjtytutorial/blocks/FirstBlockTile.java
+++ src/main/java/com/poboy975/mcjtytutorial/blocks/FirstBlockTile.java
@@ -1,14 +1,18 @@
 package com.poboy975.mcjtytutorial.blocks;
 
+import kotlin.Lazy;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.INBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.INBTSerializable;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nonnull;
@@ -18,7 +22,7 @@ import static com.poboy975.mcjtytutorial.blocks.ModBlocks.FIRSTBLOCK_TILE;
 
 public class FirstBlockTile extends TileEntity implements ITickableTileEntity{
 
-    private ItemStackHandler handler;
+    private LazyOptional<IItemHandler> handler = LazyOptional.of(this::createHandler);
 
     public FirstBlockTile() {
         super(FIRSTBLOCK_TILE);
@@ -32,37 +36,36 @@ public class FirstBlockTile extends TileEntity implements ITickableTileEntity{
     @Override
     public void read(CompoundNBT tag) {
         CompoundNBT invTag = tag.getCompound("inv");
-        getHandler().deserializeNBT(invTag);
+        handler.ifPresent(h -> ((INBTSerializable<CompoundNBT>)h).deserializeNBT(invTag));
         super.read(tag);
     }
 
     // this writes the inventory to save if tile broken
     @Override
     public CompoundNBT write(CompoundNBT tag) {
-        CompoundNBT compound = getHandler().serializeNBT();
-        tag.put("inv", compound);
+        handler.ifPresent(h-> {
+            CompoundNBT compound = ((INBTSerializable<CompoundNBT>)h).serializeNBT();
+            tag.put("inv", compound);
+        });
         return super.write(tag);
     }
 
-    private ItemStackHandler getHandler() {
-        if (handler == null) {
-            handler = new ItemStackHandler(1) {
-                @Override
-                public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
-                    return stack.getItem() == Items.DIAMOND; // diamond power generator
-                }
+    private IItemHandler createHandler() {
+        return new ItemStackHandler(1) {
+            @Override
+            public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+                return stack.getItem() == Items.DIAMOND; // diamond power generator
+            }
 
-                @Nonnull
-                @Override
-                public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
-                    if (stack.getItem() != Items.DIAMOND) {
-                        return stack;
-                    }
-                    return super.insertItem(slot, stack, simulate);
+            @Nonnull
+            @Override
+            public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
+                if (stack.getItem() != Items.DIAMOND) {
+                    return stack;
                 }
-            };
-        }
-        return handler;
+                return super.insertItem(slot, stack, simulate);
+            }
+        };
     }
 
     // capabilities, add functionality, like inventory, requesting or pushing items into blocks etc.
@@ -70,7 +73,7 @@ public class FirstBlockTile extends TileEntity implements ITickableTileEntity{
     @Override
     public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
         if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
-            return LazyOptional.of(() -> (T) getHandler());
+            return handler.cast();
 
         return super.getCapability(cap, side);
     }
